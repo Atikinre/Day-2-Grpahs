debc779dd71d08c37837ff2009bd48e184698285
Glitch (hello-webpage) <none>
2020-07-01 07:33:54 +0000

    🎱🚅 Checkpoint ./script.js:18290274/5512

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
- 
-
-class Graphics1d{
-  constructor(xmin=-10.0,xmax=10.0, ymin=-10.0, ymax=10.0, W=120 * 4, H=100 * 4, f = function(x){return x*x - 9}){
+class Graphics1d {
+  constructor(
+    xmin = -10.0,
+    xmax = 10.0,
+    ymin = -10.0,
+    ymax = 10.0,
+    W = 120 * 4,
+    H = 100 * 4,
+    f = function(x) {
+      return x * x - 9;
+    }
+  ) {
     this.xmin = xmin;
     this.xmax = xmax;
     this.ymin = ymin;
@@ -9,21 +17,30 @@ class Graphics1d{
     this.W = W;
     this.H = H;
     this.f = f;
+    this.ev = 0;
   }
-  evaluate(){
-    let values = new Map;
-    for(let i = this.xmin; i <= this.xmax; i += 0.01){
-      values[i] = this.f(i);
+  evaluate() {
+    this.values = new Map();
+    for (let i = this.xmin; i <= this.xmax + 0.1; i += 0.01) {
+      this.values[i] = this.f(i);
     }
-    return values;
+    console.log(this.f(this.xmax));
+    this.ev = 1;
+    return this.values;
   }
-  draw(dots = "red", axis = "green", zeros = "indigo", gaps = "magenta", bg = "gray"){
+  draw(
+    dots = "red",
+    axis = "green",
+    zeros = "indigo",
+    gaps = "magenta",
+    bg = "gray"
+  ) {
     var graph = document.getElementById("canvas");
     var ctx = graph.getContext("2d");
     var drawed = new Graphics1d();
-    let values = this.evaluate();
-    ctx.fillStyle = bg; 
-    ctx.fillRect(0, 0, ng.W, ng.H)
+    if (this.ev == 0) this.evaluate();
+    ctx.fillStyle = bg;
+    ctx.fillRect(0, 0, ng.W, ng.H);
     ctx.beginPath();
     ctx.strokeStyle = axis;
     ctx.moveTo(0, ng.H / 2);
@@ -34,29 +51,57 @@ class Graphics1d{
     ctx.stroke();
     ctx.lineWidth = 0.2;
     ctx.strokeStyle = axis;
-    for(let i = 0; i <= ng.W; i += ng.W / (Math.abs(ng.xmin) + Math.abs(ng.xmax)))
-      for(let j = 0; j <= ng.H; j += ng.H / (Math.abs(ng.ymin) + Math.abs(ng.ymax))){
-      ctx.beginPath();
-      ctx.moveTo(i, j);
-      ctx.lineTo(i + ng.W / (Math.abs(ng.xmin) + Math.abs(ng.xmax)), j);
-      ctx.lineTo(i + ng.W / (Math.abs(ng.xmin) + Math.abs(ng.xmax)), j + ng.H / (Math.abs(ng.ymin) + Math.abs(ng.ymax)));
-      ctx.lineTo(i, j + ng.H / (Math.abs(ng.ymin) + Math.abs(ng.ymax)));
-      ctx.closePath();
-      ctx.stroke();
+    for (
+      let i = 0;
+      i <= ng.W;
+      i += ng.W / (Math.abs(ng.xmin) + Math.abs(ng.xmax))
+    )
+      for (
+        let j = 0;
+        j <= ng.H;
+        j += ng.H / (Math.abs(ng.ymin) + Math.abs(ng.ymax))
+      ) {
+        ctx.beginPath();
+        ctx.moveTo(i, j);
+        ctx.lineTo(i + ng.W / (Math.abs(ng.xmin) + Math.abs(ng.xmax)), j);
+        ctx.lineTo(
+          i + ng.W / (Math.abs(ng.xmin) + Math.abs(ng.xmax)),
+          j + ng.H / (Math.abs(ng.ymin) + Math.abs(ng.ymax))
+        );
+        ctx.lineTo(i, j + ng.H / (Math.abs(ng.ymin) + Math.abs(ng.ymax)));
+        ctx.closePath();
+        ctx.stroke();
       }
     ctx.beginPath();
     ctx.lineWidth = 1;
     ctx.strokeStyle = dots;
-    ctx.moveTo(ng.W / 2 + ng.xmin * ng.W / 2 / ng.xmax, ng.H/2 - values[ng.xmin] * ng.H / 2 / ng.ymax);
-    for(let i = ng.xmin; i <= ng.xmax; i += 0.01)
-      ctx.lineTo(ng.W / 2 + i * ng.W / 2 / ng.xmax  , (ng.H/2 - values[i] * ng.H / 2 / ng.ymax));
+    ctx.moveTo(
+      ng.W / 2 + (ng.xmin * ng.W) / 2 / ng.xmax,
+      ng.H / 2 - (this.values[ng.xmin] * ng.H) / 2 / ng.ymax
+    );
+    for (let i = ng.xmin; i <= ng.xmax; i += 0.01)
+      ctx.lineTo(
+        ng.W / 2 + (i * ng.W) / 2 / ng.xmax,
+        ng.H / 2 - (this.values[i] * ng.H) / 2 / ng.ymax
+      );
     ctx.closePath();
     ctx.stroke();
   }
-  
-  autodraw(){
-    
+
+  autodraw(
+    dots = "red",
+    axis = "green",
+    zeros = "indigo",
+    gaps = "magenta",
+    bg = "gray"
+  ) {
+    if (this.ev == 0) this.evaluate();
+    console.log(this.ymin, this.ymax);
+    this.ymin = this.values[this.xmin];
+    this.ymax = this.values[this.xmax];
+    console.log(this.ymin, this.ymax);
+    this.draw(dots, axis, zeros, gaps, bg);
   }
-};
+}
 var ng = new Graphics1d();
-ng.draw();
+ng.autodraw();
