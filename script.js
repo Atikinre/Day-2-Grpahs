cf272ce9f0914d1dceccbf859b1b129ffe6e8ba5
Glitch (hello-webpage) <none>
2020-07-01 06:44:10 +0000

    🎎🚪 Checkpoint ./script.js:18290274/460 ./index.html:18290274/40

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
  
 
 class Graphics1d{
-  constructor(xmin=-10.0,xmax=10.0, ymin=-10.0, ymax=10.0, W=120, H=100, f = function(x){return x*x - 9}){
+  constructor(xmin=-10.0,xmax=10.0, ymin=-10.0, ymax=10.0, W=120 * 4, H=100 * 4, f = function(x){return x*x - 9}){
     this.xmin = xmin;
     this.xmax = xmax;
     this.ymin = ymin;
@@ -23,10 +23,22 @@ class Graphics1d{
     var drawed = new Graphics1d();
     let values = this.evaluate();
     ctx.beginPath();
-    for(let i = this.xmin; i <= this.xmax; i += 0.01)
-      ctx.lineTo(60 + i * ng.W / 2 / ng.xmax  , (50 + values[i] * ng.H / 2 / ng.ymax));
+    ctx.strokeStyle = "green";
+    ctx.moveTo(0, ng.H / 2);
+    ctx.lineTo(ng.W, ng.H / 2);
+    ctx.moveTo(ng.W / 2, 0);
+    ctx.lineTo(ng.W / 2, ng.H);
     ctx.closePath();
     ctx.stroke();
+    ctx.strokeWidth = 2;
+    ctx.beginPath();
+    for(int i = 0; i <= )
+    ctx.beginPath();
+    ctx.moveTo(ng.xmin, values[ng.xmin]);
+    for(let i = ng.xmin; i <= ng.xmax; i += 0.01)
+      ctx.lineTo(ng.W / 2 + i * ng.W / 2 / ng.xmax  , (ng.H - values[i] * ng.H / 2 / ng.ymax));
+    ctx.closePath();
+    
   }
   
   autodraw(){
