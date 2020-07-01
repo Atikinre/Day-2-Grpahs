3ed8c867d421aad5b77c27d073b49456c4436363
Atikinre <67623174+Atikinre@users.noreply.github.com>
2020-07-01 14:48:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 class Graphics1d {
   constructor(
     xmin = -10.0,
-    xmax = 100.0,
-    ymin = -100.0,
-    ymax = 100.0,
-    W = 120 * 4,
-    H = 100 * 4,
+    xmax = 10.0,
+    ymin = -10.0,
+    ymax = 10.0,
+    W = 1000,
+    H = 1000,
     f = function(x) {
-      return 10*Math.sin(x);
+      return x * x - 9;
     }
   ) {
     this.xmin = xmin;
@@ -21,8 +21,12 @@ class Graphics1d {
   }
   evaluate() {
     this.values = new Map();
-    
-    for (let i = this.xmin; i <= this.xmax; i += (-this.xmin + this.xmax) / this.W) {
+
+    for (
+      let i = this.xmin;
+      i <= this.xmax;
+      i += (-this.xmin + this.xmax) / this.W
+    ) {
       this.values[i] = this.f(i);
     }
     this.ev = 1;
@@ -36,6 +40,8 @@ class Graphics1d {
     bg = "gray"
   ) {
     var graph = document.getElementById("canvas");
+    graph.width = this.W;
+    graph.height = this.H;
     var ctx = graph.getContext("2d");
     var drawed = new Graphics1d();
     if (this.ev == 0) this.evaluate();
@@ -56,8 +62,8 @@ class Graphics1d {
     ctx.stroke();
     ctx.lineWidth = 0.2;
     ctx.strokeStyle = axis;
-    for (let i = 0; i <= ng.W; i += stepx)
-      for (let j = 0; j <= ng.H; j += stepy) {
+    for (let i = 0; i < ng.W; i += stepx)
+      for (let j = 0; j < ng.H; j += stepy) {
         ctx.beginPath();
         ctx.moveTo(i, j);
         ctx.lineTo(i + stepx, j);
@@ -70,7 +76,11 @@ class Graphics1d {
     ctx.lineWidth = 1;
     ctx.strokeStyle = dots;
     ctx.moveTo(zerox + this.xmin * stepx, zeroy - this.f(this.xmin) * stepy);
-    for (let i = this.xmin; i <= this.xmax; i += (-this.xmin + this.xmax) / this.W) {
+    for (
+      let i = this.xmin;
+      i <= this.xmax;
+      i += (-this.xmin + this.xmax) / this.W
+    ) {
       if (0) {
         ctx.stroke();
         ctx.closePath();
@@ -78,8 +88,15 @@ class Graphics1d {
         ctx.fillStyle = zeros;
         ctx.arc(
           zerox + i * stepx,
-          zeroy - stepy * this.values[i - 0.1],
+          zeroy - stepy * this.values[i - (-this.xmin + this.xmax) / this.W],
           3,
+          0,
+          180
+        );
+        ctx.arc(
+          zerox + i * stepx,
+          zeroy - stepy * this.values[i + (-this.xmin + this.xmax) / this.W],
+          stepx,
           0,
           180
         );
@@ -92,20 +109,17 @@ class Graphics1d {
     }
     ctx.stroke();
     ctx.closePath();
-    ctx.font = stepx +" serif";
+    ctx.font = stepx + "px Consolas";
+    ctx.textBaseline = "ideographic";
     ctx.fillStyle = "black";
     let mx = "(" + this.xmax + ", " + this.ymax + ")",
-        mn = "(" + this.xmin + ", " + this.ymin + ")";
+      mn = "(" + this.xmin + ", " + this.ymin + ")";
     ctx.fillText(
       mx,
-      zerox + this.xmax * stepx - stepx * (mx.length + 1),
-      zeroy + this.ymin * stepy + stepx * mx.length
+      zerox + this.xmax * stepx - (stepx * mx.length) / 1.8,
+      zeroy + this.ymin * stepy + stepx
     );
-    ctx.fillText(
-      mn,
-      zerox + this.xmin * stepx,
-      zeroy + this.ymax * stepy - stepy
-    );
+    ctx.fillText(mn, zerox + this.xmin * stepx, zeroy + this.ymax * stepy);
   }
 
   autodraw(
@@ -117,9 +131,51 @@ class Graphics1d {
   ) {
     this.ymin = this.f(this.xmin);
     this.ymax = this.f(this.xmax);
-
     this.draw(dots, axis, zeros, gaps, bg);
   }
 }
+function replaceSpecialSequence(str) {
+  str = str.split("cos").join("Math.cos");
+  str = str.split("sin").join("Math.sin");
+  str = str.split("tan").join("Math.tan");
+  str = str.split("aMath.cos").join("Math.acos");
+  str = str.split("aMath.sin").join("Math.asin");
+  str = str.split("aMath.tan").join("Math.atan");
+  str = str.split("pi").join("Math.PI");
+  str = str.split("ln2").join("Math.LN2");
+  str = str.split("ln10").join("Math.LN10");
+  str = str.split("log2e").join("Math.LOG2E");
+  str = str.split("log10e").join("Math.LOG10E");
+  str = str.split("sqrt1_2").join("Math.SQRT1_2");
+  str = str.split("sqrt2").join("Math.SQRT2");
+  str = str.split("abs").join("Math.abs");
+  str = str.split("ceil").join("Math.ceil");
+  str = str.split("exp").join("Math.exp");
+  str = str.split("floor").join("Math.floor");
+  str = str.split("ln").join("Math.log");
+  str = str.split("max").join("Math.max");
+  str = str.split("min").join("Math.min");
+  str = str.split("pow").join("Math.pow");
+  str = str.split("round").join("Math.round");
+  str = str.split("lg").join("logab");
+  str = str.split("sqrt").join("Math.sqrt");
+  str = str.split("e").join("Math.E");
+  return str;
+}
 var ng = new Graphics1d();
 ng.draw();
+function yes() {
+  var xmin = parseFloat(document.getElementById("xmin").value),
+    xmax = parseFloat(document.getElementById("xmax").value),
+    ymin = parseFloat(document.getElementById("ymin").value),
+    ymax = parseFloat(document.getElementById("ymax").value),
+    W = parseFloat(document.getElementById("W").value),
+    H = parseFloat(document.getElementById("H").value),
+    f = document.getElementById("f").value;
+  f = replaceSpecialSequence(f);
+  var m = function(x) {
+    return eval(f);
+  };
+  ng = new Graphics1d(xmin, xmax, ymin, ymax, W, H, m);
+  ng.draw();
+}
