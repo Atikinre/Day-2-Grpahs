80577411ff0e531c792012b2dadf729bec7f573a
Glitch (prairie-royal-snowboard) <none>
2020-07-01 09:28:45 +0000

    💎🙏 Updated with Glitch

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 class Graphics1d {
   constructor(
     xmin = -10.0,
-    xmax = 10.0,
-    ymin = -10.0,
-    ymax = 10.0,
+    xmax = 100.0,
+    ymin = -100.0,
+    ymax = 100.0,
     W = 120 * 4,
     H = 100 * 4,
     f = function(x) {
-      return 1/x;
+      return 10*Math.sin(x);
     }
   ) {
     this.xmin = xmin;
@@ -21,7 +21,8 @@ class Graphics1d {
   }
   evaluate() {
     this.values = new Map();
-    for (let i = this.xmin; i <= this.xmax; i += 0.01) {
+    
+    for (let i = this.xmin; i <= this.xmax; i += (-this.xmin + this.xmax) / this.W) {
       this.values[i] = this.f(i);
     }
     this.ev = 1;
@@ -38,11 +39,10 @@ class Graphics1d {
     var ctx = graph.getContext("2d");
     var drawed = new Graphics1d();
     if (this.ev == 0) this.evaluate();
-    let 
-    stepx = this.W / (-this.xmin + this.xmax),
-    stepy = this.H / (-this.ymin + this.ymax),
-    zerox = Math.abs(this.xmin) * stepx,
-    zeroy = Math.abs(this.ymin) * stepy;
+    let stepx = this.W / (-this.xmin + this.xmax),
+      stepy = this.H / (-this.ymin + this.ymax),
+      zerox = Math.abs(this.xmin) * stepx,
+      zeroy = Math.abs(this.ymin) * stepy;
     ctx.fillStyle = bg;
     ctx.fillRect(0, 0, ng.W, ng.H);
     ctx.beginPath();
@@ -56,23 +56,12 @@ class Graphics1d {
     ctx.stroke();
     ctx.lineWidth = 0.2;
     ctx.strokeStyle = axis;
-    for (
-      let i = 0;
-      i <= ng.W;
-      i += stepx
-    )
-      for (
-        let j = 0;
-        j <= ng.H;
-        j += stepy
-      ) {
+    for (let i = 0; i <= ng.W; i += stepx)
+      for (let j = 0; j <= ng.H; j += stepy) {
         ctx.beginPath();
         ctx.moveTo(i, j);
         ctx.lineTo(i + stepx, j);
-        ctx.lineTo(
-          i + stepx,
-          j + stepy
-        );
+        ctx.lineTo(i + stepx, j + stepy);
         ctx.lineTo(i, j + stepy);
         ctx.closePath();
         ctx.stroke();
@@ -80,34 +69,43 @@ class Graphics1d {
     ctx.beginPath();
     ctx.lineWidth = 1;
     ctx.strokeStyle = dots;
-    ctx.moveTo(
-      zerox + (this.xmin * stepx),
-      zeroy - (this.f(this.xmin) * stepy));
-    for (let i = this.xmin; i <= this.xmax; i += 0.01){
-      if(this.values[i] > 0 && this.values[i] < 10e-10){
+    ctx.moveTo(zerox + this.xmin * stepx, zeroy - this.f(this.xmin) * stepy);
+    for (let i = this.xmin; i <= this.xmax; i += (-this.xmin + this.xmax) / this.W) {
+      if (0) {
         ctx.stroke();
         ctx.closePath();
         ctx.beginPath();
         ctx.fillStyle = zeros;
-        ctx.arc(zerox + i*stepx, zeroy - stepy * this.values[i - 0.1], 3, 0, 180);
+        ctx.arc(
+          zerox + i * stepx,
+          zeroy - stepy * this.values[i - 0.1],
+          3,
+          0,
+          180
+        );
         ctx.fill();
         ctx.closePath();
         ctx.beginPath();
-      }
-      else{
-      console.log(i, this.values[i]);
-      ctx.lineTo(
-        zerox + i * stepx,
-        zeroy - this.values[i] * stepy
-      );
+      } else {
+        ctx.lineTo(zerox + i * stepx, zeroy - this.values[i] * stepy);
       }
     }
     ctx.stroke();
     ctx.closePath();
-    ctx.font = '20px serif';
+    ctx.font = stepx +" serif";
     ctx.fillStyle = "black";
-    ctx.fillText('(' + this.xmax + ", " + this.ymax + ')', zerox + this.xmax * stepx - 70, zeroy + this.ymin * stepy + 20);
-    ctx.fillText('(' + this.xmin + ", "+ this.ymin + ')', zerox + this.xmin * stepx , zeroy + this.ymax * stepy - 10);
+    let mx = "(" + this.xmax + ", " + this.ymax + ")",
+        mn = "(" + this.xmin + ", " + this.ymin + ")";
+    ctx.fillText(
+      mx,
+      zerox + this.xmax * stepx - stepx * (mx.length + 1),
+      zeroy + this.ymin * stepy + stepx * mx.length
+    );
+    ctx.fillText(
+      mn,
+      zerox + this.xmin * stepx,
+      zeroy + this.ymax * stepy - stepy
+    );
   }
 
   autodraw(
@@ -117,10 +115,9 @@ class Graphics1d {
     gaps = "magenta",
     bg = "gray"
   ) {
-    
     this.ymin = this.f(this.xmin);
     this.ymax = this.f(this.xmax);
-    
+
     this.draw(dots, axis, zeros, gaps, bg);
   }
 }
