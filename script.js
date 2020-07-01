046142270adb2ba2f88e050755cff46f8e80ac7c
Glitch (hello-webpage) <none>
2020-07-01 05:45:20 +0000

    🚩👙 Checkpoint ./script.js:18290274/1310

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-class Graphics1d{
-  xmin : 0,
+ 
 
+class Graphics1d{
+  constructor(xmin=-10.0,xmax=10.0, ymin=-10.0, ymax=10.0, W=120, H=100, f = function(x){return x*x - 9}){
+    this.xmin = xmin;
+    this.xmax = xmax;
+    this.ymin = ymin;
+    this.ymax = ymax;
+    this.W = W;
+    this.H = H;
+    this.f = f;
+  }
+  evaluate(){
+    
+  }
+  draw(){
+    
+  }
   
+  autodraw(){
+    
+  }
 };
+  
